2c35b233c9458d84370e61cdeabfd79c38dc2eec
Francesco Suriano <fra.suri94@gmail.com>
2015-04-28 17:44:35 +0200

    Commit di prova

--- src/ClusterSet.java
+++ src/ClusterSet.java
@@ -2,7 +2,6 @@
  * Insieme dei cluster ottenuti da una tabella delle transazioni
  */
 
-
 class ClusterSet {
 	
 	//Attributi
@@ -14,7 +13,7 @@ class ClusterSet {
 	//Metodi
 	
 	/**
-	 * Costruttore di default
+	 * Crea un ClusterSet vuoto
 	 */
 	ClusterSet()
 	{
--- src/MainTest.java
+++ src/MainTest.java
@@ -4,9 +4,8 @@ public class MainTest {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		
-		
+	public static void main(String[] args) 
+	{
 		Data data =new Data();
 		System.out.println(data);
 		double radius = 2.0;
